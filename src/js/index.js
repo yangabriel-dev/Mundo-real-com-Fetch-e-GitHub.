83757be8863dc679b8921d6a0f577f3270e01839
yangabriel-dev <yangabriel2.0.yg@gmail.com>
2025-05-25 01:03:39 -0300

    repositorios do usuário

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,10 @@ async function user(userName){
     const response = await fetch (`https://api.github.com/users/${userName}`)
     return await response.json()
 }
-
+async function repos(userName){
+    const response = await fetch (`https://api.github.com/users/${userName}/repos`)
+    return await response.json()
+}
 function getUserProfile(userName){
     user(userName).then(userData =>{
         let userInfo =`<div class="info">
@@ -32,5 +35,18 @@ function getUserProfile(userName){
                         </div>`
         document.querySelector('.profile-data').innerHTML = userInfo
         getUserRepositories(userName)
+    })
+}
+function getUserRepositories(userName) {
+    repos(userName).then(reposData => {
+        let repositoriesItens = ""
+        reposData.forEach(repo => {
+            repositoriesItens += `<li><a href="${repo.html_url}"target="_blank">${repo.name}, ${repo.forks}🍴, ${repo.watchers}👀, ${repo.stargazers_count}⭐, ${repo.language}👩🏾‍💻</a></li>`;
+        })
+        document.querySelector('.profile-data').innerHTML += 
+        `<div class="repositories section">
+            <h2>Repositórios</h2>
+            <ul>${repositoriesItens}</ul>
+        </div>`;
     })
 }
